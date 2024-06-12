af5b51f90e90779f7d37b6cb35359c70a04b5b2d
cmaynard <chrissy.swe.maynard@gmail.com>
2024-06-11 20:48:34 -0400

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Typography, Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+
+const Header = ({ title, subTitle }) => {
+  const theme = useTheme();
+  const colors = useTokens(theme.colors.mode);
+  return (
+    <Box>
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: "5px" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.green[400]}>
+        {subTitle}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      ></Box>
+      <Header title="DASHBOARD" subTitle="Manage Your Dashboard" />
+      Dashboard
+    </Box>
+  );
+};
+
+export default Dashboard;
